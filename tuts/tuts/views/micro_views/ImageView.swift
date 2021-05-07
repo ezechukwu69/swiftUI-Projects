acb9e516135faba7b204f41cca1759334f8051d6
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-05-07 23:39:59 +0100

    Second commit

---
+++ tuts/tuts/views/micro_views/ImageView.swift
@@ -0,0 +1,31 @@
+//
+//  ImageView.swift
+//  tuts
+//
+//  Created by Apple on 07/05/2021.
+//
+
+import SwiftUI
+import Combine
+
+
+
+
+struct ImageView: View {
+    
+    @StateObject var vm: ImageViewModel
+    
+    init(url: String) {
+        _vm = StateObject(wrappedValue: ImageViewModel(url: url))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+            }
+        }
+        
+    }
+}
