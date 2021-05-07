79541070923d28a5d47494a3a0f185e18eb91a83
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-05-07 10:45:32 +0100

    Initial Commit

---
+++ swiftUi/swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiApp.swift
+//  swiftUi
+//
+//  Created by Apple on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
